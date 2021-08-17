15493b49440a27705d498f3f5eda6bad2753b4f7
BrennerFerreira <brennercsferreira@gmail.com>
2021-08-17 13:28:31 -0300

    test: add tests to sum_services function

---
+++ sum_services.test.js
@@ -0,0 +1,20 @@
+const sumAllMultiplesOf3And5SmallerThanValue = require("./sum_services");
+
+test(`should return the sum of all numbers smaller than the informed number
+    when parameter is a valid number`, () => {
+  expect(sumAllMultiplesOf3And5SmallerThanValue(10)).toBe(23);
+  expect(sumAllMultiplesOf3And5SmallerThanValue(11)).toBe(33);
+  expect(sumAllMultiplesOf3And5SmallerThanValue(0)).toBe(0);
+  expect(sumAllMultiplesOf3And5SmallerThanValue(1)).toBe(0);
+  expect(sumAllMultiplesOf3And5SmallerThanValue(2)).toBe(0);
+  expect(sumAllMultiplesOf3And5SmallerThanValue(3)).toBe(0);
+  expect(sumAllMultiplesOf3And5SmallerThanValue(20)).toBe(78);
+  expect(sumAllMultiplesOf3And5SmallerThanValue(50)).toBe(543);
+});
+
+test("should return undefined when parameter is not a number", () => {
+  expect(sumAllMultiplesOf3And5SmallerThanValue("test string")).toBe(undefined);
+  expect(sumAllMultiplesOf3And5SmallerThanValue(true)).toBe(undefined);
+  expect(sumAllMultiplesOf3And5SmallerThanValue({})).toBe(undefined);
+  expect(sumAllMultiplesOf3And5SmallerThanValue([])).toBe(undefined);
+});
